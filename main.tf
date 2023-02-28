d6f2b39afe354b646b571c3c079922a74a889ff6
ChaitanyaKottha <chythanya.k@gmail.com>
2023-03-01 00:39:51 +0530

    updating roboshop-infra

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-resource "null_resource" "test"{
-  provisioner "local-exec" {
-    command = " echo ${var.env}"
-  }
+#resource "null_resource" "test"{
+#  provisioner "local-exec" {
+#    command = " echo ${var.env}"
+#  }
+#}
+
+module "network" {
+  source = "github.com/ChaitanyaKottha/tf-module-vpc"
+  version= "main"
 }
